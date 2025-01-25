431e4db72de1c7bd07f82b814b44b1989944bb80
nick0124 <nikita.popov@lindenvalley.de>
2025-01-25 13:43:00 +0500

    Add bubbles movement

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -7,10 +7,14 @@ public class Bubble : MonoBehaviour
     public float _increaseScale = 0.001f;
     public float _clickIncreaseScale = 0.1f;
     public float _destroyScale = 1;
+    private Rigidbody2D _rb;
+    public Vector2 _velocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        AddForceAtRandomDirection();
+
         _destroyScale = Random.Range(1f, 5f);
         _increaseScale = Random.Range(_increaseScale, _increaseScale * 5);
 
@@ -31,10 +35,28 @@ public class Bubble : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        _velocity = _rb.linearVelocity;
+
+        if (Mathf.Abs(_rb.linearVelocity.x) < 0.02f || Mathf.Abs(_rb.linearVelocity.y) < 0.02f)
+        {
+            AddForceAtRandomDirection();
+        }
     }
 
     void OnMouseDown()
     {
         transform.localScale += new Vector3(_clickIncreaseScale, _clickIncreaseScale, _clickIncreaseScale);
+    }
+
+    private void AddForceAtRandomDirection()
+    {
+        if (_rb == null) _rb = GetComponent<Rigidbody2D>();
+
+        float length = 1f;
+        Vector2 randomVector = Random.insideUnitCircle.normalized * length;
+
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.AddForce(randomVector, ForceMode2D.Impulse);
     }
 }
