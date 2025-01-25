c6988c18da482ff2fefb6f17d1d79cb0b55d988e
nick0124 <nikita.popov@lindenvalley.de>
2025-01-25 13:10:41 +0500

    Add random to bubbles

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+
     public float _increaseScale = 0.001f;
     public float _clickIncreaseScale = 0.1f;
     public float _destroyScale = 1;
@@ -10,7 +11,11 @@ public class Bubble : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _destroyScale = Random.Range(1f,5f);
+        _destroyScale = Random.Range(1f, 5f);
+        _increaseScale = Random.Range(_increaseScale, _increaseScale * 5);
+
+        float initialScale = Random.Range(0.5f, 1.2f);
+        transform.localScale = new Vector3(initialScale, initialScale, initialScale);
     }
 
     // Update is called once per frame
@@ -22,7 +27,8 @@ public class Bubble : MonoBehaviour
     void FixedUpdate()
     {
         transform.localScale += new Vector3(_increaseScale, _increaseScale, _increaseScale);
-        if(transform.localScale.x > _destroyScale){
+        if (transform.localScale.x > _destroyScale)
+        {
             Destroy(gameObject);
         }
     }
